b22f98743d686bdd8783744b7ecb75519470b0fe
emilianomacias <emilianomacias0@gmail.com>
2016-08-09 11:03:37 -0500

    Validation

--- client/main.js
+++ client/main.js
@@ -105,7 +105,7 @@ Template.lists.helpers({
 
 Template.register.events({
     'submit form':function(event){
-        event.preventDefault();
+       /* event.preventDefault();
         var email = $('[name=email]').val();
         var password = $('[name=password]').val();
         Accounts.createUser({
@@ -117,9 +117,32 @@ Template.register.events({
             } else {
                 Router.go('home');
             }
-        });        
+        });        */
     }
 });
+Template.register.onRendered(function(){
+    var validator=$('.register').validate({
+        submitHandler:function(event){
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            email:email,
+            password:password
+        },function(){
+            if (error) {
+             if(error.reason == "Email already exists."){
+                validator.showErrors({
+                    email: "That email already belongs to a registered user."   
+                });
+             }
+            } else {
+                Router.go('home');
+            }
+        });         
+        }
+    });
+});
+
 
 
 Template.navigation.events({
@@ -133,7 +156,7 @@ Template.navigation.events({
 Template.login.events({
     'submit form':function(event){
         event.preventDefault();
-        var email=$('[name=email]').val();
+       /* var email=$('[name=email]').val();
         var password = $('[name=password]').val();
         Meteor.loginWithPassword(email,password,function(error){
             if (error) {
@@ -146,9 +169,70 @@ Template.login.events({
                     Router.go(currentRoute);
                 }
             } 
-        });
+        });*/
     }
+});
+
+Template.login.onCreated(function(){
+    console.log("The 'login' template was just created.");
+});
+
+Template.login.onRendered(function(){
+    var validator=$('.login').validate({
+        submitHandler:function(event){
+       var email=$('[name=email]').val();
+        var password = $('[name=password]').val();
+        Meteor.loginWithPassword(email,password,function(error){
+            if (error) {
+               if(error.reason == "User not found"){
+                    validator.showErrors({
+                        email: error.reason    
+                    });
+                }
+                if(error.reason == "Incorrect password"){
+                    validator.showErrors({
+                        password: error.reason    
+                    });
+                }
+            } else {
+                //Se obtiene la ruta alctal que el usuario esta 
+                var currentRoute = Router.current().route.getName();
+                //Direccionamos al aruta actual
+                if (currentRoute=="login") {
+                    Router.go('home');
+                }
+            } 
+        });
+        }
+    });
+});
+
+
+Template.login.onDestroyed(function(){
+    console.log("The 'login' template was just destroyed.");
 });
 
 
 
+$.validator.setDefaults({
+    rules: {
+        email: {
+            required: true,
+            email: true
+        },
+        password: {
+            required: true,
+            minlength: 6
+        }
+    },
+    messages: {
+        email: {
+            required: "You must enter an email address.",
+            email: "You've entered an invalid email address."
+        },
+        password: {
+            required: "You must enter a password.",
+            minlength: "Your password must be at least {0} characters."
+        }
+    }
+});
